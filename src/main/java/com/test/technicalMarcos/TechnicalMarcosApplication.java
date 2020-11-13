62a0ebba8038bc12ccf8570be48848a1b9e039ad
Marcos <markis992@gmail.com>
2020-11-13 10:57:48 +0100

    first commit

---
+++ src/main/java/com/test/technicalMarcos/TechnicalMarcosApplication.java
@@ -0,0 +1,13 @@
+package com.test.technicalMarcos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnicalMarcosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechnicalMarcosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/technicalMarcos/TechnicalMarcosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.technicalMarcos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnicalMarcosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
